4f32fee497a824409fcfceb0318813ee42939d57
Tran Le Quoc <tlquoc03@gmail.com>
2024-05-18 23:35:12 +0700

    allow_host_ for deploying

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-lwk$ks6rez)z6o8py=y7#6*$f*knd#8)senks9m=do1u*$lbm+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['project-8stw.onrender.com']
 
 
 # Application definition
